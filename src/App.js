9e270be3868ab4e19dee858641507ebcb08c1ee2
SoJW <118089050+JW-Choi-4631@users.noreply.github.com>
2023-04-29 16:40:54 +0900

    초기화 오류 수정

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
       }
       setCard([...card, newCard]);
       console.log(card);
-      setTitle('제목을 입력하세요.(최대 20자)'');
+      setTitle('제목을 입력하세요.(최대 20자)');
     }
    
   };
